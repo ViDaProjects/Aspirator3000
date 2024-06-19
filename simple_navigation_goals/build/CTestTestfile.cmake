609de7498083891221d52d77079518f699412b7a
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2024-06-19 00:25:09 -0300

    Nicolas made objective position action caller

---
+++ simple_navigation_goals/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nicolas/catkin_ws/simple_navigation_goals
+# Build directory: /home/nicolas/catkin_ws/simple_navigation_goals/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
